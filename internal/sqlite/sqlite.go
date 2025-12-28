005a5fe2042e6a6d6c2bd9c4414f25489e93f165
Parth Kshirsagar <parthyk007@gmail.com>
2025-12-29 02:52:34 +0530

    add GET endpoint to retrieve all students and implement corresponding storage method

--- internal/sqlite/sqlite.go
+++ internal/sqlite/sqlite.go
@@ -74,3 +74,32 @@ func (s *SQLite) GetStudentById(id int64) (types.Student, error) {
 
 	return student, nil
 }
+
+func (s *SQLite) GetStudents() ([]types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students")
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+	
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+		if err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Age); err != nil {
+			return nil, err
+		}
+
+		students = append(students, student)
+	}
+
+	return students, nil
+}
